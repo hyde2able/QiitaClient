93446bd99bf07214c15f48249d840fb37fdf586a
pokohide <hyde31415@gmail.com>
2016-12-23 03:13:59 +0900

    initial commit

---
+++ Qiiter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Qiiter
+//
+//  Created by 酒井英伸 on 2016/12/23.
+//  Copyright © 2016年 pokohide. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
